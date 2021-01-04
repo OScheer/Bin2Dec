7dd3f7ff3478b92948243ab3e7f47416fae3ae5b
[your <o.t.scheer@gmail.com>
2021-01-04 20:57:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bin2dec/Extensions.kt
@@ -0,0 +1,32 @@
+package com.example.bin2dec
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+val pattern = Regex("^[0-1]{1,}\$")
+
+fun EditText.validate(message: String, validator: (String) -> Boolean) {
+    this.afterTextChanged {
+        this.error = if (validator(it)) null else message
+    }
+    this.error = if (validator(this.text.toString())) null else message
+}
+
+fun String.isValidBinary(): Boolean
+        = this.isNotEmpty() && pattern.containsMatchIn(this)
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object: TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            afterTextChanged.invoke(s.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+    })
+}
+
---
+++ app/src/main/java/com/example/bin2dec/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.bin2dec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    private fun convertBinToDec(bin: Long): Int {
+        var bin = bin
+        var dec = 0
+        var i = 0
+        var remain: Long
+
+        while (bin.toInt() != 0) {
+            remain = bin % 10
+            bin /= 10
+            dec += (remain * 2.0.pow(i.toDouble())).toInt()
+            ++i
+        }
+        return dec
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        inputText.validate("No Binary Number") { input -> input.isValidBinary() }
+
+        confirm_button.setOnClickListener {
+            if (inputText.text.toString().trim().isNotEmpty()) {
+                val inputBin = inputText.text.toString().toLong()
+                outputBinText.text = inputBin.toString()
+                outputDecText.text = convertBinToDec(inputBin).toString()
+                inputText.text.clear()
+            } else {
+                Toast.makeText(this, "Binary Input is Empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
